5d7511b6de356f340e35cf56fb638c6cda423013
erivan123erivan <96080412+erivan123erivan@users.noreply.github.com>
2023-04-08 13:34:29 +0200

    sdsd
    dssd

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const server = require("http").createServer(app);
+const router = express.Router();
 env.config();
+
+app.use("/uploads", express.static("uploads"));
+
+const apiRoutes = require("./routes/api");
+app.use("/api", apiRoutes);
 
 const io = require("socket.io")(server, {
   cors: {
@@ -12,12 +18,14 @@ const io = require("socket.io")(server, {
   },
 });
 
+router.get("/", (req, res) => {
+  res.send("Welcome to the API!");
+});
+
 const authRoutes = require("./routes/auth");
 const socketRouter = require("./routes/socketRouter")(io);
 const messageRoutes = require("./routes/message");
 const uploadRoutes = require("./routes/uploads")
-
-
 
 mongoose
   .connect(
@@ -31,6 +39,10 @@ mongoose
     console.log("Database connected");
   });
 
+app.get('/', (req, res) => {
+  res.send('Welcome to my app!');
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("uploads"));
@@ -39,8 +51,6 @@ app.use("/api", socketRouter);
 app.use("/api", messageRoutes);
 app.use("/api", uploadRoutes);
 
-
-
 io.on("connection", (socket) => {
   console.log(`ID: ${socket.id}`);
 });
@@ -48,3 +58,5 @@ io.on("connection", (socket) => {
 server.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${ process.env.PORT }`);
 });
+
+module.exports = router;
